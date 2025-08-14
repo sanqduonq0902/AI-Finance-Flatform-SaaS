3e1bcfad7356e1b987271ff71bd7aa5b9d1492eb
SanqDuonq <namsang0902s@gmail.com>
2025-08-15 06:27:05 +0700

    project setup

---
+++ backend/config/http.config.ts
@@ -0,0 +1,23 @@
+const httpConfig = () => ({
+    OK: 200,
+    CREATED: 201,
+    ACCEPTED: 202,
+    NO_CONTENT: 204,
+
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    METHOD_NOT_ALLOWED: 405,
+    CONFLICT: 409,
+    UNPROCESSABLE_ENTITY: 422,
+    TOO_MANY_REQUESTS: 429,
+
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_IMPLEMENTED: 501,
+    BAD_GATEWAY: 502,
+    SERVICES_UNAVAILABLE: 503,
+    GATEWAY_TIMEOUT: 504
+});
+
+export const HTTP_STATUS = httpConfig();
+export type HttpStatusCodeType = (typeof HTTP_STATUS)[keyof typeof HTTP_STATUS];
---
+++ backend/index.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import { Env } from './config/env.config';
+import cors from 'cors';
+import { HTTP_STATUS } from './config/http.config';
+
+const app = express();
+const BASE_PATH = Env.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(
+    cors({
+        origin: Env.FRONTEND_ORIGIN,
+        credentials: true
+    })
+);
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(HTTP_STATUS.OK).json({
+        message: 'Server is running'
+    });
+});
+
+app.listen(Env.PORT, () => {
+    console.log(`App started at http://localhost:${Env.PORT}`);
+});
+
