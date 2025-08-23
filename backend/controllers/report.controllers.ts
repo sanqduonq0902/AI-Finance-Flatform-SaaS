9d3b30542432eecba4109efb29cc1fb94ab9854a
SanqDuonq <namsang0902s@gmail.com>
2025-08-23 12:38:15 +0700

    feat: update setting report

--- backend/controllers/report.controllers.ts
+++ backend/controllers/report.controllers.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/async-handler.middleware";
 import { HTTP_STATUS } from "../config/http.config";
-import { getAllReportsService } from "../services/report.services";
+import { getAllReportsService, updateReportSettingService } from "../services/report.services";
+import { updateReportSettingSchema } from "../validations/report.validation";
 
 export const getAllReportsController = asyncHandler(
     async (req: Request, res: Response) => {
@@ -16,6 +17,17 @@ export const getAllReportsController = asyncHandler(
         return res.status(HTTP_STATUS.OK).json({
             message: 'Reports history fetched successfully',
             result
+        })
+    }
+)
+
+export const updateReportSettingController = asyncHandler(
+    async (req: Request, res: Response) => {
+        const userId = req.user?._id;
+        const body = updateReportSettingSchema.parse(req.body);
+        await updateReportSettingService(userId, body);
+        return res.status(HTTP_STATUS.OK).json({
+            message: 'Reports setting updated successfully'
         })
     }
 )
--- backend/services/report.services.ts
+++ backend/services/report.services.ts
@@ -1,4 +1,8 @@
+import ReportSettingModel from "../models/report-setting.model";
 import ReportModel from "../models/report.model";
+import { NotFoundException } from "../utils/app-error";
+import { calculateNextReportDate } from "../utils/helper";
+import { UpdateReportSettingType } from "../validations/report.validation";
 
 export const getAllReportsService = async (userId: string, pagination: {
     pageSize: number, 
@@ -25,4 +29,33 @@ export const getAllReportsService = async (userId: string, pagination: {
             skip
         }
     }
+}
+
+export const updateReportSettingService = async (userId: string, body: UpdateReportSettingType) => {
+    const { isEnabled } = body;
+    let nextReportDate = null;
+
+    const existingReport = await ReportSettingModel.findOne({
+        userId
+    });
+
+    if (!existingReport) throw new NotFoundException('Report setting not found');
+
+    if (isEnabled) {
+        const currentNextReportDate = existingReport.nextReportDate;
+        const now = new Date();
+        if (!currentNextReportDate || currentNextReportDate <= now) {
+            nextReportDate = calculateNextReportDate(
+                existingReport.lastSentDate
+            )
+        } else {
+            nextReportDate = currentNextReportDate
+        }
+    }
+    console.log(nextReportDate, 'nextReportDate');
+    existingReport.set({
+        ...body,
+        nextReportDate
+    })
+    await existingReport.save();
 }
